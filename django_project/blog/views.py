983c59b3d274bd197c2c7cae1e08da9809aef18c
handreygalon <handrey.eg@gmail.com>
2019-02-27 13:36:03 -0300

    # Added base.html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Handrey',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 26, 2019'
+    },
+    {
+        'author': 'Fulano',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 27, 2019'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
