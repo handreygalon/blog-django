b3e7326574e3ef89dd185ebfcc84f5878ac36a8f
handreygalon <handrey.eg@gmail.com>
2019-02-27 18:21:52 -0300

    # Created models and deleted dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Handrey',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'February 26, 2019'
-    },
-    {
-        'author': 'Fulano',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 27, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
